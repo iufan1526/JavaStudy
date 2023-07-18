36c095f378c6e40ccc4347c94fe1fc6487791bc0
KimSeungTae <iufan1526@gmail.com>
2023-07-18 20:28:00 +0900

    인터페이스 DefualtMethod 추가

--- src/interfacestudy/DefualtMethodInterface.java
+++ src/interfacestudy/DefualtMethodInterface.java
@@ -3,8 +3,13 @@ package interfacestudy;
 public interface DefualtMethodInterface {
 
     final static int COUNTRY_CODE = 50;
+    final static String COUNTRY_CODE_NAME = "한국";
 
     default int getCountryCode() {
         return DefualtMethodInterface.COUNTRY_CODE;
+    }
+
+    default String getName() {
+        return DefualtMethodInterface.COUNTRY_CODE_NAME;
     }
 }
